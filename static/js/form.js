a981c65584a7fc97bca08c1835f85de721c93755
juandarov <juandarov@gmail.com>
2017-03-08 10:25:11 +0100

    form en constrution

---
+++ static/js/form.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+    $('#myForm').validator()
+
+    // $('#inputPassword').pwstrength(options);
+    // var options = {
+    //     minChar: 5,
+    // };
+});
+
+
+// CLEAR FORM
+function myFunction() {
+    document.getElementById("myForm").reset();
+}
+// END OF CLEAR FORM FUNTION
