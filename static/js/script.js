c6657a91cbaae019172d16bb13aac190e139428e
solene <solenantoine@gmail.com>
2017-03-08 10:22:01 +0100

    avancé css

---
+++ static/js/script.js
@@ -0,0 +1,31 @@
+$(document).ready(function(){
+    $("#slides").slidesjs({
+        width: 940,
+        height: 528,
+        play: {
+            active: true,
+        // [boolean] Generate the play and stop buttons.
+        // You cannot use your own buttons. Sorry.
+            effect: "slide",
+        // [string] Can be either "slide" or "fade".
+            interval: 4000,
+        // [number] Time spent on each slide in milliseconds.
+            auto: true,
+        // [boolean] Start playing the slideshow on load.
+            swap: true,
+        // [boolean] show/hide stop and play buttons
+            pauseOnHover: false,
+        // [boolean] pause a playing slideshow on hover
+            restartDelay: 2500,
+        // [number] restart delay on inactive slideshow
+    },
+        pagination: {
+            active: false,
+        // [boolean] Create pagination items.
+        // You cannot use your own pagination. Sorry.
+        effect: "fade",
+        // [string] Can be either "slide" or "fade".
+    },
+      });
+
+})
