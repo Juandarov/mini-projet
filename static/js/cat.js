3f8614ef602e8facb890f12118af1a608da8c666
solene <solenantoine@gmail.com>
2017-03-09 17:11:02 +0100

    pas grand chose

--- static/js/cat.js
+++ static/js/cat.js
@@ -8,12 +8,17 @@ $(document).ready(function() {
         var monTitre = $('<h3 class="name">'+catalog[i].name+'</h3>');
         var maDescription = $('<p class="plant">'+catalog[i].description+'<a class="btn btn-default" href="produit.html?produit='+[i]+'" role="button">En savoir plus</a></p>');
         var monPrix = $('<p class="prix">'+catalog[i].price+' €</p>');
-            $("#catalogue").append(maDiv);
-            maDiv.append(monTitre);
-            maDiv.append(maSection);
-            maSection.append(monImage);
-            maSection.append(maDescription);
-            maSection.append(monPrix);
+        $("#catalogue").append(maDiv);
+        maDiv.append(monTitre);
+        maDiv.append(maSection);
+        maSection.append(monImage);
+        maSection.append(maDescription);
+        maSection.append(monPrix);
 
+        // $("#vingt").click(function() {
+        //     if (monPrix < 30) {
+        //     }
+
+        })
     }
 })
