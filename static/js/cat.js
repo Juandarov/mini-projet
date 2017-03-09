4e4321f439480172596b6be88099886b8570404c
solene <solenantoine@gmail.com>
2017-03-09 16:01:21 +0100

    fice produit ok

--- static/js/cat.js
+++ static/js/cat.js
@@ -1,9 +1,3 @@
-// var hackerList = new List('hacker-list', options);
-//
-// var options = {
-//     valueNames: [ 'name', 'plant' ]
-// };
-
 
 $(document).ready(function() {
 
@@ -13,7 +7,7 @@ $(document).ready(function() {
         var maSection = $('<section></section>')
         var monImage = $('<img src='+catalog[i].thumb+'></img>');
         var monTitre = $('<h3 class="name">'+catalog[i].name+'</h3>');
-        var maDescription = $('<p class="plant">'+catalog[i].description+'<a class="btn btn-default" href="produit.html" role="button">En savoir plus</a></p>');
+        var maDescription = $('<p class="plant">'+catalog[i].description+'<a class="btn btn-default" href="produit.html?produit='+[i]+'" role="button">En savoir plus</a></p>');
         var monPrix = $('<p class="prix">'+catalog[i].price+' €</p>');
             $("#catalogue").append(maDiv);
             maDiv.append(monTitre);
--- static/js/produit.js
+++ static/js/produit.js
@@ -1,3 +1,13 @@
 $(document).ready(function() {
-    console.log(GET_PARAM("name"));
+    var monTitre = $('<h3 class="name">'+catalog[GET_PARAM("produit")].name+'</h3>');
+    $("#fiche").append(monTitre);
+    var monImage = $('<img src='+catalog[GET_PARAM("produit")].pictures+'></img>');
+    $("#fiche").append(monImage);
+    var maDescription = $('<p class="plant">'+catalog[GET_PARAM("produit")].description+'</p>');
+    $("#fiche").append(maDescription);
+    var monPrix = $('<p class="prix">'+catalog[GET_PARAM("produit")].price+' €</p>');
+    $("#fiche").append(monPrix);
+    var monPanier = $('<button type="button" class="btn btn-success"><i class="fa fa-shopping-basket" aria-hidden="true"></i> Ajouter au panier</button>')
+    $("#fiche").append(monPanier);
+
 })
