39df72b9b0f179e58fae65c1d17bcacd006217e7
solene <solenantoine@gmail.com>
2017-03-10 15:13:05 +0100

    filtres ok

--- static/js/cat.js
+++ static/js/cat.js
@@ -1,24 +1,59 @@
 $(document).ready(function() {
 
     var maDiv = $('<div class="produit col-md-10"></div>');
+    var prixAComparer = ""
 
     for (var i = 0; i < catalog.length; i++) {
+        var maDivProduit = $('<div class="le_produit"></div>')
         var maSection = $('<section></section>')
         var monImage = $('<img src='+catalog[i].thumb+'></img>');
         var monTitre = $('<h3 class="name">'+catalog[i].name+'</h3>');
-        var maDescription = $('<p class="plant">'+catalog[i].description+'<a class="btn btn-default" href="produit.html?produit='+[i]+'" role="button">En savoir plus</a></p>');
-        var monPrix = $('<p class="prix">'+catalog[i].price+' €</p>');
+        var maDescription = $('<p class="plant">'+catalog[i].description+'<a class="btn btn-default" href="produit.html?produit='+[i]+'"role="button">En savoir plus</a></p>');
+        var monPrix = $('<p class="prix">'+catalog[i].price+'</p>€');
         $("#catalogue").append(maDiv);
-        maDiv.append(monTitre);
-        maDiv.append(maSection);
+        maDiv.append(maDivProduit);
+        maDivProduit.append(monTitre);
+        maDivProduit.append(maSection);
         maSection.append(monImage);
         maSection.append(maDescription);
         maSection.append(monPrix);
-
-        // $("#vingt").click(function() {
-        //     if (monPrix < 30) {
-        //     }
-
-        // })
     }
+
+    $("#vingt").click(function() {
+        var lesDivProduit = $(".le_produit");
+        for (var i = 0; i < lesDivProduit.length; i++) {
+            var leProduit = $(lesDivProduit[i])
+            prixAComparer = leProduit.find(".prix").html()
+
+            if (prixAComparer > 31) {
+                console.log(prixAComparer);
+                leProduit.hide();
+            }
+            else {
+                leProduit.show();
+            }
+        }
+    })
+    $("#trente").click(function() {
+        var lesDivProduit = $(".le_produit");
+        for (var i = 0; i < lesDivProduit.length; i++) {
+            var leProduit = $(lesDivProduit[i])
+            leProduit.show();
+            prixAComparer = leProduit.find(".prix").html()
+
+            if (prixAComparer > 41) {
+                console.log(prixAComparer);
+                leProduit.hide();
+            }
+            else {
+                leProduit.show();
+            }
+
+        }
+    })
+    $("#tous").click(function() {
+        var lesDivProduit = $(".le_produit");
+            var leProduit = $(lesDivProduit[i])
+            lesDivProduit.show();
+    })
 })
