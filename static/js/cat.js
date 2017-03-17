d7b84d0c9e97c96ebf7994b805ddf00b55d9b736
juandarov <juandarov@gmail.com>
2017-03-17 10:27:58 +0100

    panier en ajax

--- static/js/cat.js
+++ static/js/cat.js
@@ -1,23 +1,36 @@
 $(document).ready(function() {
+    $.ajax({
+
+      url : 'https://codi-e-commerce.herokuapp.com/',
+
+      type : 'GET',
+
+      dataType : 'json',
+
+      success : function(catalog){ // code_html contient le HTML renvoyé
+
+          for (var i = 0; i < catalog.length; i++) {
+              var maDivProduit = $('<div class="le_produit"></div>')
+              var maSection = $('<section></section>')
+              var monImage = $('<img src=' + catalog[i].thumb + '></img>');
+              var monTitre = $('<h3 class="name">' + catalog[i].name + '</h3>');
+              var maDescription = $('<p class="plant">' + catalog[i].description + '<a class="btn btn-default" href="produit.html?produit=' + [i] + '"role="button">En savoir plus</a></p>');
+              var monPrix = $('<p class="prix">' + catalog[i].price + '€' + '</p>');
+              $("#catalogue").append(maDiv);
+              maDiv.append(maDivProduit);
+              maDivProduit.append(monTitre);
+              maDivProduit.append(maSection);
+              maSection.append(monImage);
+              maSection.append(maDescription);
+              maSection.append(monPrix);
+          }
+      }
+
+   });
 
     var maDiv = $('<div class="produit col-md-10"></div>');
     var prixAComparer = ""
 
-    for (var i = 0; i < catalog.length; i++) {
-        var maDivProduit = $('<div class="le_produit"></div>')
-        var maSection = $('<section></section>')
-        var monImage = $('<img src=' + catalog[i].thumb + '></img>');
-        var monTitre = $('<h3 class="name">' + catalog[i].name + '</h3>');
-        var maDescription = $('<p class="plant">' + catalog[i].description + '<a class="btn btn-default" href="produit.html?produit=' + [i] + '"role="button">En savoir plus</a></p>');
-        var monPrix = $('<p class="prix">' + catalog[i].price + '€' + '</p>');
-        $("#catalogue").append(maDiv);
-        maDiv.append(maDivProduit);
-        maDivProduit.append(monTitre);
-        maDivProduit.append(maSection);
-        maSection.append(monImage);
-        maSection.append(maDescription);
-        maSection.append(monPrix);
-    }
 
 
 
